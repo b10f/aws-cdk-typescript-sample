b25d18a7f8ccbc896361a5c2f6f36e39f0c8d894
Rudolf Bárdos <bardos@outlook.com>
2024-06-25 18:02:47 +0200

    feat: add hitcounter construct

--- lib/aws-cdk-typescript-sample-stack.ts
+++ lib/aws-cdk-typescript-sample-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class AwsCdkTypescriptSampleStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -8,14 +9,18 @@ export class AwsCdkTypescriptSampleStack extends cdk.Stack {
 
         // defines an AWS Lambda resource
         const hello = new lambda.Function(this, 'HelloHandler', {
-            runtime: lambda.Runtime.NODEJS_20_X,    // execution environment
+            runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
             code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
             handler: 'hello.handler'                // file is "hello", function is "handler"
         });
 
+        const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+            downstream: hello
+        });
+
         // defines an API Gateway REST API resource backed by our "hello" function.
         new apigw.LambdaRestApi(this, 'Endpoint', {
-            handler: hello
+            handler: helloWithCounter.handler
         });
 
     }
