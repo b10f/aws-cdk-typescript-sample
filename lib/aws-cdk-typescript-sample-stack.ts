a506b03285b2fbf3aea414f57af660041d770417
Rudolf Bárdos <bardos@outlook.com>
2024-06-25 17:32:40 +0200

    feat: add API gateway

--- lib/aws-cdk-typescript-sample-stack.ts
+++ lib/aws-cdk-typescript-sample-stack.ts
@@ -1,16 +1,22 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
-export class AwsCdkTypescriptSampleStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsCdkTypescriptSampleStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // defines an AWS Lambda resource
         const hello = new lambda.Function(this, 'HelloHandler', {
-            runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
+            runtime: lambda.Runtime.NODEJS_20_X,    // execution environment
             code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
             handler: 'hello.handler'                // file is "hello", function is "handler"
         });
+
+        // defines an API Gateway REST API resource backed by our "hello" function.
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: hello
+        });
+
     }
 }
